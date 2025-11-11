8a6339aa4abe74e5e0dc15b30123195529a76ece
arth1312 <arthkoradiya@gmail.com>
2025-11-11 13:12:02 +0530

    pr-7_Admin-panel-setup-Sendotp

--- pr-7_Admin-panel-setup-sendOTP/config/DB_connection.js
+++
@@ -1,9 +0,0 @@
-const mongoose =   require("mongoose")
-
-const DB_connection = async (req, res) => {
-    await mongoose.connect("mongodb+srv://arth1312:Arth1312@cluster0.t7hd3z1.mongodb.net/admin-panel")
-    .then("connection successfully ")
-    .catch((err) =>  console.log(err))
-}
-
-module.exports = DB_connection;
